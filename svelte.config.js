5131e1fc8cbfd8a3304e99672f55b36fff4eac71
Ethan <ecb266@nau.edu>
2023-11-29 19:39:50 -0700

    please

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import dotenv from 'dotenv';
+import azure from 'svelte-adapter-azure-swa';
 
 dotenv.config();
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: azure(),
   },
   preprocess: vitePreprocess(),
 };
