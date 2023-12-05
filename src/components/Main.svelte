aff7c5d1b17f735507a331cce066301c8259c383
Ethan <ecb266@nau.edu>
2023-12-05 15:25:24 -0700

    re-design of projects and experiences and sm edits

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -47,8 +47,12 @@
       description: "For my senior capstone project, I led a team in designing and building a modular curtained clean room area. The goal was to establish sterile manufacturing capabilities for stroke treatment medical devices while converting the existing clean room into a gowning room. Our project focused on creating a modular clean room that could be easily disassembled and reassembled. We successfully met requirements such as FFU compatibility, positive pressure, space for 6 people, and 10' x 10' floor space per module. Our project contributed to optimizing the workflow and ensuring a sterile environment for medical device assembly.",
       buttonLabel: "Learn More",
       buttonLink: "https://www.ceias.nau.edu/capstone/projects/ME/2024/F23toSp24_CleanRoom/index.html",
-      imageSrc: "images/cleanroom_des.jpeg",
-      imageAlt: "Clean Room Design"
+      images: [
+        {
+            src: "/images/cleanroom_des.jpeg",
+            alt: "Clean Room Design 1"
+        }
+      ]
     },
       {
         title: "IDEA Lab Manager",
@@ -56,8 +60,12 @@
         description: "As one of the original members and designers of the IDEA Lab at NAU, I played an instrumental in enhancing its functionality and creating a welcoming environment for students. My contributions included a range of responsibilities, including creating work order forms, establishing lab rules, expanding machine offerings, repairing 3D printers, training team members, organizing machine information, managing data, coordinating with faculty, ensuring lab safety, educating students on manufacturing methods, providing design feedback, ordering materials, creating informative displays, and maintaining communication with safety staff. The lab's success is a direct result of my proactive approach and dedication as one of the lab's founders and designers.",
         buttonLabel: "Learn More",
         buttonLink: "https://ceias.nau.edu/me/idealab/",
-        imageSrc: "",
-        imageAlt: ""
+        images: [
+        {
+            src: "/images/cleanroom_des.jpeg",
+            alt: "Clean Room Design 1"
+        }
+      ]
       },
       {
         title: "E3 Displays",
@@ -65,15 +73,25 @@
         description: "During my internship at E3, I had the opportunity to contribute to a significant project focused on the improvement of a debonder device. This Capstone project, funded by E3 and developed at NAU, aimed to remove glass from digital display electrical components. However, upon delivery, the debonder proved to be inefficient and bulky. My role involved reimagining the design and functionality of the debonder to address its shortcomings. Through careful analysis and innovative thinking, I successfully redesigned the device, making it more compact and efficient in its operation. Additionally, I had the chance to contribute to various aspects of the project, including the design and manufacturing of sheet metal jigs to house electrical components, conducting dry optical bonding testing, and working in the company's cleanroom for product testing and manufacturing. Throughout my internship at E3, I gained valuable experience in problem-solving, design optimization, and collaboration within a professional engineering environment.",
         buttonLabel: "Learn More",
         buttonLink: "https://www.e3displays.com/about-us/",
-        imageSrc: "",
-        imageAlt: ""
+        images: [
+        {
+            src: "/images/cleanroom_des.jpeg",
+            alt: "Clean Room Design 1"
+        }
+      ]
       },
       {
         title: "Future Engineers Summer Camp",
         sub: "Northern Arizona University 2022",
         description: "During my time as a Mechanical Engineering New Graduate, I had the opportunity to design and host two impactful summer camps at NAU. The first camp, held in 2021, was a Girls in STEM summer camp aimed at introducing young girls to the exciting world of engineering. The main objective was to break gender stereotypes and showcase that engineering is not limited to boys. Through engaging activities and hands-on projects, the camp provided a solid foundation in engineering fundamentals. Building on the success of the previous year, I organized a second summer camp in 2022 that expanded its reach to elementary students from a reservation. This camp delved deeper into design principles and placed a strong emphasis on robotics. Over the course of a week, the students were immersed in the process of designing, building, and racing LEGO robots. By providing them with practical experience in engineering, we aimed to inspire and empower these young minds to pursue their interests in the field.",
         buttonLabel: "Learn More",
-        buttonLink: "#"
+        buttonLink: "#",
+        images: [
+        {
+            src: "/images/cleanroom_des.jpeg",
+            alt: "Clean Room Design 1"
+        }
+      ]
       }
     ];
 
@@ -135,37 +153,37 @@
     </section>
     <section class="py-20 lg:py-32 flex flex-col gap-24 transition ease-in-out" id="projects">
         <div class="flex flex-col gap-2 text-center">
-            <h6 class="text-large sm:text-xl md:text-2xl">
+            <h6 class="font-light text-large sm:text-xl md:text-2xl">
                 Engineering Proficiencies at a Glance
             </h6>
-            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Discover My <span class="poppins text-lime-500">Technical Expertise</span>
+            <h3 class="font-light text-3xl sm:text-4xl md:text-5xl">
+                Discover My <span class="poppins text-green-700">Technical Expertise</span>
             </h3>
         </div>
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
             <Step step={steps[0]}>
                 <p class="text-lg">
                     Operational experience with machines such as Stratasy Printers<strong
-                        class="text-lime-500">(Fortus 400/250 and J35 PolyJet)</strong
+                        class="text-green-700">(Fortus 400/250 and J35 PolyJet)</strong
                     >,
-                    <strong class="text-lime-500">Markforged Mark Two</strong
+                    <strong class="text-green-700">Markforged Mark Two</strong
                     >, and
-                    <strong class="text-lime-500">Bosslaser</strong> Lasercutter
+                    <strong class="text-green-700">Bosslaser</strong> Lasercutter
                 </p>
             </Step>
             <Step step={steps[1]}>
                 <p class="text-lg">
                     Strong exposure to various programming languages such as <strong
-                        class="text-lime-500">Python</strong
+                        class="text-green-700">Python</strong
                     >,
-                    <strong class="text-lime-500">MATLAB</strong
+                    <strong class="text-green-700">MATLAB</strong
                     > and
-                    <strong class="text-lime-500">HTML/CSS</strong>
+                    <strong class="text-green-700">HTML/CSS</strong>
                 </p>
             </Step>
             <Step step={steps[2]}>
                 <p class="text-lg">
-                    Ask me about the software I use! Such as, <strong class="text-lime-500"
+                    Ask me about the software I use! Such as, <strong class="text-green-700"
                         >Solidworks, LightBurn, GrabCAD, Reality Composer, </strong
                     > and more!
                 </p>
@@ -173,23 +191,116 @@
         </div>
     </section>
 
-    {#each projects as project, i}
-    <div class="{i % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} mb-4 project-box">
-      <div class="w-full bg-slate-50 p-4">
-        <h2 class="text-2xl font-semibold">{project.title}</h2>
-        <p class="text-lg mt-2">{project.sub}</p>
-        {#if project.buttonLabel && project.buttonLink}
-          <a href="{project.buttonLink}" class="mt-4 inline-block px-4 py-2 bg-lime-500 text-white rounded hover:bg-slate-500">{project.buttonLabel}</a>
-        {/if}
-      </div>
-      <div class="w-full bg-slate-100 p-4">
-        {#if project.imageSrc && project.imageAlt}
-          <img src="{project.imageSrc}" alt="{project.imageAlt}" class="responsive-image">
-        {/if}
-        <p class="mt-2">{project.description}</p>
-      </div>
-    </div>
-    {/each}
+<!--         {#each projects as project, i}
+        <div class="project-container">
+        <div class="project-description">
+            <h2 class="text-2xl font-light">{project.title}</h2>
+            <p class="text-lg mt-2">{project.sub}</p>
+            {#if project.buttonLabel && project.buttonLink}
+            <a href="{project.buttonLink}" class="mt-4 inline-block px-4 py-2 bg-green-700 text-white rounded hover:bg-slate-500">{project.buttonLabel}</a>
+            {/if}
+            <p class="mt-2">{project.description}</p>
+        </div>
+        <div class="project-images">
+            {#each project.images as image}
+            <img src="{image.src}" alt="{image.alt}" class="project-image">
+            {/each}
+        </div>
+        </div>
+        {/each} -->
+
+        <section class="bg-gray-50">
+            <div class="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+<!--                 <div>
+                    <h2 class="text-3xl font-bold tracki text-center sm:text-5xl">Test H2</h2>
+                    <p class="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">Testing for the p tag</p>
+                </div> -->
+                <div class="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                    <div>
+                        <h3 class="text-2xl font-bold tracki sm:text-3xl">Modular Clean Room Design - Aneuvas Technologies</h3>
+                        <p class="mt-3 text-lg dark:text-gray-400">Northern Arizona University Mechanical Engineering Capstone 2023-2024</p>
+                        <div class="mt-12 space-y-12">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-green-700 dark:text-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-7 h-7">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg font-medium leadi">Design</h4>
+                                    <p class="mt-2 dark:text-gray-400">&#8226; Led the design and construction of a modular curtained clean room area</p>
+                                    <p class="mt-2 dark:text-gray-400">&#8226; Created a modular clean room for sterile manufacturing of medical devices</p>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center w-12 h-12 rounded-md dark:bg-green-700 dark:text-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-7 h-7">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg font-medium leadi">Technicalities</h4>
+                                    <p class="mt-2 dark:text-gray-400">&#8226; Met requirements such as FFU compatibility, positive pressure, and space for 6 people</p>
+                                    <p class="mt-2 dark:text-gray-400">&#8226; Optimized workflow and ensured a sterile environment for medical device assembly</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div aria-hidden="true" class="mt-10 lg:mt-0">
+                        <img src="/images/cleanroom_des.jpeg" alt="" class="mx-auto rounded-lg shadow-lg dark:bg-gray-500">
+                        <br>
+                        <img src="/images/team.jpg" alt="" class="mx-auto rounded-lg shadow-lg dark:bg-gray-500">
+
+                    </div>
+                </div>
+                <div>
+                    <div class="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                        <div class="lg:col-start-2">
+                            <h3 class="text-2xl font-bold tracki sm:text-3xl ">IDEA Lab Manager</h3>
+                            <p class="mt-3 text-lg dark:text-gray-400">Northern Arizona University 2020 - 2023</p>
+                            <div class="mt-12 space-y-12">
+                                <div class="flex">
+                                    <div class="flex-shrink-0">
+                                        <div class="flex items-center justify-center w-12 h-12 rounded-md  dark:bg-green-700 dark:text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-7 h-7">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div class="ml-4">
+                                        <h4 class="text-lg font-medium leadi ">Leadership</h4>
+                                        <p class="mt-2 dark:text-gray-400">&#8226; Played a key role in establishing and enhancing the functionality of the IDEA Lab</p>
+                                        <p class="mt-2 dark:text-gray-400">&#8226; Managed lab rules, machine offerings, and repairs</p>
+                                    </div>
+                                </div>
+                                <div class="flex">
+                                    <div class="flex-shrink-0">
+                                        <div class="flex items-center justify-center w-12 h-12 rounded-md  dark:bg-green-700 dark:text-white">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-7 h-7">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div class="ml-4">
+                                        <h4 class="text-lg font-medium leadi ">Technicalities</h4>
+                                        <p class="mt-2 dark:text-gray-400">&#8226; Created work order forms, organized machine information, and managed data</p>
+                                        <p class="mt-2 dark:text-gray-400">&#8226; Coordinated with faculty, ensured lab safety, and provided design feedback</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+                            <img src="/images/IDEA-lab.jpg" alt="" class="mx-auto rounded-lg shadow-lg dark:bg-gray-500">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     <section
         id="about"
@@ -202,12 +313,9 @@
                 Want to know more?
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                A bit <span class="poppins text-lime-500">about</span> me.
+                A bit <span class="poppins text-green-700">about</span> me.
             </h3>
         </div>
-        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
-            I am . . .
-        </p>
         <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
             {#each benefits as benefit, index}
                 <div class="flex gap-6 sm:gap-8">
@@ -257,7 +365,7 @@
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Your message</label>
                     <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                 </div>
-                <button type="submit" class="py-3 px-5 text-sm font-semibold text-center text-slate-50 rounded-lg bg-lime-500 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                <button type="submit" class="py-3 px-5 text-sm font-semibold text-center text-slate-50 rounded-lg bg-green-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
             </form>
         </div>
       </section> -->
